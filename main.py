4a6510d10ad6f7cacf734cf50410977f9ae0636e
Drew Hund <languageinvestigator@gmail.com>
2026-04-23 15:49:26 -0400

    base request to news api

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+res = requests.get(
+    os.getenv("REQUEST_URL"),
+    headers={"User-Agent": "Mozilla/5.0"}
+)
+content = res.json()
+articles = content["articles"]
+
+for article in content["articles"]:
+    print(article["title"])
